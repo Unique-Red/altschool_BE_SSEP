55b645733cfe7b8e2c4f0653411ba77689554fa2
Unique-Red <vnoah410@gmail.com>
2022-11-01 01:06:22 +0100

    structure

--- app.py
+++ app.py
@@ -1,9 +1,5 @@
-from blog import app, db
-from app import User
+from blog import app
 
-# @app.shell_context_processor
-# def make_shell_context():
-#     return dict(db=db, User=User)
 
 if __name__ == "__main__":
     app.run(debug=True)
