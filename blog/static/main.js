3af0e5cb91cfd164c2f387c2b133c44fffb67265
Unique-Red <vnoah410@gmail.com>
2022-11-05 01:57:30 +0100

    modifications done for home

---
+++ blog/static/main.js
@@ -0,0 +1,11 @@
+const like1 = document.getElementById('like1')
+const like2 = document.getElementById('like2')
+const like3 = document.getElementById('like3')
+const like4 = document.getElementById('like4')
+
+
+like1.addEventListener("click",(e)=>{e.preventDefault()})
+like2.addEventListener("click",(e)=>{e.preventDefault()})
+like3.addEventListener("click",(e)=>{e.preventDefault()})
+like4.addEventListener("click",(e)=>{e.preventDefault()})
+console.log('js active')
